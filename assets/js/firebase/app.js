1a4bd81a6752dc5167cb01f5e2a5d1d69627d70b
anapbds <justtley@gmail.com>
2022-08-31 00:42:06 -0300

    JS e Firebase

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,32 @@
+ // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyApy_SI_9Zl_JeCwJ4mQcv8Ig-9izkVivY",
+
+    authDomain: "landingpage-mundo-invert-b3487.firebaseapp.com",
+
+    projectId: "landingpage-mundo-invert-b3487",
+
+    storageBucket: "landingpage-mundo-invert-b3487.appspot.com",
+
+    messagingSenderId: "687400236655",
+
+    appId: "1:687400236655:web:9706d4b007831e1eb47f9a"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+  export default app
